73e3adc09530f0366f9d4686c243c7793d572968
ek-ole <ek_34@mail.ru>
2024-12-15 22:33:20 +0300

    fix(advanced-1.2): add attribute alt to images

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ var createNewTaskElement=function(taskString){
   //button.delete
   var deleteButton=document.createElement("button");//delete button
   var deleteButtonImg=document.createElement("img");//delete button image
+  deleteButtonImg.alt="Remove button";
 
   label.innerText=taskString;
   label.className='task';
